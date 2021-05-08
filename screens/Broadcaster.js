39b9c41d1234cfbfb6dede5582c28fb3d6fc06d5
saiashish9 <saiashish3760@gmail.com>
2021-05-09 00:04:54 +0530

    implemented

--- screens/Broadcaster.js
+++ screens/Broadcaster.js
@@ -9,6 +9,8 @@ import {
 } from 'react-native';
 
 import FontAwesome from 'react-native-vector-icons/FontAwesome5';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import {NodeCameraView} from 'react-native-nodemediaclient';
 
 const {width, height} = Dimensions.get('window');
@@ -16,14 +18,14 @@ const {width, height} = Dimensions.get('window');
 const config = {
   cameraConfig: {
     cameraId: 1,
-    cameraFrontMirror: false,
+    cameraFrontMirror: true,
   },
   videoConfig: {
     preset: 4,
     bitrate: 2000000,
     profile: 2,
     fps: 30,
-    videoFrontMirror: true,
+    videoFrontMirror: false,
   },
   audioConfig: {
     bitrate: 128000,
@@ -34,6 +36,7 @@ const config = {
 
 const Broadcaster = () => {
   const [play, shouldPlay] = useState(false);
+  const [flash, flashEnable] = useState(false);
   const cameraViewRef = React.useRef(null);
   const streamKey = 'cd649d75-0ecc-df40-ca22-55db95254262';
   const url = `rtmps://global-live.mux.com:443/app/${streamKey}`;
@@ -56,15 +59,39 @@ const Broadcaster = () => {
       <TouchableOpacity
         style={styles.icon}
         onPress={() => {
-          if (!play) {
-            cameraViewRef.current.start();
-          } else {
-            cameraViewRef.current.stop();
-          }
+          // if (!play) {
+          //   cameraViewRef.current.start();
+          // } else {
+          //   cameraViewRef.current.stop();
+          // }
+          cameraViewRef.current.switchCamera();
           shouldPlay(!play);
         }}>
         <FontAwesome
           name={`${play ? 'pause' : 'play'}-circle`}
+          color="#fff"
+          size={45}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.flash}
+        onPress={() => {
+          if (!flash) {
+            cameraViewRef.current.flashEnable(true);
+          } else {
+            cameraViewRef.current.flashEnable(false);
+          }
+          flashEnable(!flash);
+        }}>
+        <Ionicons name={flash ? 'flash' : 'flash-off'} color="#fff" size={45} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.camera}
+        onPress={() => {
+          cameraViewRef.current.switchCamera();
+        }}>
+        <Ionicons
+          name={`md-camera-reverse-outline`}
           color="#fff"
           size={45}
         />
@@ -83,6 +110,16 @@ const styles = StyleSheet.create({
   icon: {
     position: 'absolute',
     bottom: height * 0.08,
+  },
+  flash: {
+    position: 'absolute',
+    bottom: height * 0.08,
+    left: width * 0.15,
+  },
+  camera: {
+    position: 'absolute',
+    bottom: height * 0.08,
+    right: width * 0.15,
   },
   tag: {
     backgroundColor: 'red',
